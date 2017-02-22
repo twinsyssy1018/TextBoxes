c2030e04e45d949ac0848b2a2882d9e3e32d5ca2
elmar-haussmann <elmar-haussmann@gmx.net>
2017-02-22 15:10:48 +0800

    fix

--- textboxes_demo.py
+++ textboxes_demo.py
@@ -9,17 +9,12 @@ import xml.dom.minidom
 import riseml.server
 from io import BytesIO
 # %matplotlib inline
+import caffe
+import sys
 
 plt.rcParams['figure.figsize'] = (10, 10)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
-
-# Make sure that caffe is on the python path:
-caffe_root = '.'  # this file is expected to be in {caffe_root}/examples
-os.chdir(caffe_root)
-import caffe
-import sys
-sys.path.insert(0, 'python')
 
 caffe.set_device(0)
 caffe.set_mode_gpu()
